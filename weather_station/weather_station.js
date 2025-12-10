6e09d6743f4a8be204b76392366bfb74d7bfbfba
tmiguel78 <tmiguel78@yahoo.es>
2025-12-10 18:10:38 +0100

    nueva ruta para termometro e imagenes background

--- weather_station/weather_station.js
+++ weather_station/weather_station.js
@@ -48,7 +48,7 @@ const template = (data) => {
         <h2>${city}, ${country}</h2>
         <h2>${condition}</h2>
         <img src="${imagen}" alt="${condition}" id="imagen_actual"/>
-        <p class="grados_celsius">${grados_celsius}º <img src="../assets/termometro.png" alt="Termómetro" /></p>
+        <p class="grados_celsius">${grados_celsius}º <img src="https://tmiguel78.github.io/project-break-dashboard/assets/termometro.png" alt="Termómetro" /></p>
         <ul>
             <li class="datos_secundarios">Precipitaciones: ${precipitaciones} mm</li>
             <li class="datos_secundarios">Humedad: ${humedad} %</li>
